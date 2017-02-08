2157c309fb5584c755080c44183696124f5e4356
John Lee <xxjlee@oc5014436062.ibm.com>
2017-02-09 06:44:46 +1100

    Simple change

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -12,6 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+09 Feb 2017 just do some change
 */
 
 package main
